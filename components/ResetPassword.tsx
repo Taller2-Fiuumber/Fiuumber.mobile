4fce7a1fee1e038ae15d05f42646b415b2a314e0
tomasbernardotti <btomas@fi.uba.ar>
2022-09-28 18:13:16 -0300

    Formulario de ingreso

--- components/ResetPassword.tsx
+++ components/ResetPassword.tsx
@@ -1,27 +1,27 @@
-import React, { FC, ReactElement } from "react";
+// import React, { FC, ReactElement } from "react";
 
 
-export const ResetPassword: FC = (): ReactElement => {
-    return (
+// export const ResetPassword: FC = (): ReactElement => {
+//     return (
   
-      <form>
-      <label>
-        Usuario:
-        <input type="text" name="user" />
-      </label>
-      <br/>
-      <label>
-        Contraseña:
-        <input type="password" name="password" />
-      </label>
-      <br/>
-      <input type="submit" value="Ingresar" />
-      <br/>
+//       <form>
+//       <label>
+//         Usuario:
+//         <input type="text" name="user" />
+//       </label>
+//       <br/>
+//       <label>
+//         Contraseña:
+//         <input type="password" name="password" />
+//       </label>
+//       <br/>
+//       <input type="submit" value="Ingresar" />
+//       <br/>
      
     
       
-    </form> 
-    );
-  };
+//     </form> 
+//     );
+//   };
   
-  export default ResetPassword;
+//   export default ResetPassword;
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,33 +1,120 @@
-import { StyleSheet } from 'react-native';
+// import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import LogInForm from '../components/LogInForm';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+// import EditScreenInfo from '../components/EditScreenInfo';
+// import LogInForm from '../components/LogInForm';
+// import { Text, View } from '../components/Themed';
+// import { RootTabScreenProps } from '../types';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+// export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>Log In</Text>
+//       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+//       <LogInForm />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//   },
+//   separator: {
+//     marginVertical: 30,
+//     height: 1,
+//     width: '80%',
+//   },
+// });
+import React, { useState } from "react";
+//import { Auth } from "aws-amplify";
+import { useHistory } from "react-router-dom";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
+import { useFormFields } from "../libs/hooksLib";
+import { onError } from "../libs/errorLib";
+import "./ChangePassword.css";
+
+export default function ChangePassword() {
+  const history = useHistory();
+  const [fields, handleFieldChange] = useFormFields({
+    password: "",
+    oldPassword: "",
+    confirmPassword: "",
+  });
+  const [isChanging, setIsChanging] = useState(false);
+
+  function validateForm() {
+    return (
+      fields.oldPassword.length > 0 &&
+      fields.password.length > 0 &&
+      fields.password === fields.confirmPassword
+    );
+  }
+
+  async function handleChangeClick(event) {
+    event.preventDefault();
+
+    setIsChanging(true);
+
+    try {
+      const currentUser = await Auth.currentAuthenticatedUser();
+      await Auth.changePassword(
+        currentUser,
+        fields.oldPassword,
+        fields.password
+      );
+
+      history.push("/settings");
+    } catch (error) {
+      onError(error);
+      setIsChanging(false);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Log In</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <LogInForm />
-    </View>
+    <div className="ChangePassword">
+      <form onSubmit={handleChangeClick}>
+        <FormGroup bsSize="large" controlId="oldPassword">
+          <ControlLabel>Old Password</ControlLabel>
+          <FormControl
+            type="password"
+            onChange={handleFieldChange}
+            value={fields.oldPassword}
+          />
+        </FormGroup>
+        <hr />
+        <FormGroup bsSize="large" controlId="password">
+          <ControlLabel>New Password</ControlLabel>
+          <FormControl
+            type="password"
+            onChange={handleFieldChange}
+            value={fields.password}
+          />
+        </FormGroup>
+        <FormGroup bsSize="large" controlId="confirmPassword">
+          <ControlLabel>Confirm Password</ControlLabel>
+          <FormControl
+            type="password"
+            onChange={handleFieldChange}
+            value={fields.confirmPassword}
+          />
+        </FormGroup>
+        <LoaderButton
+          block
+          type="submit"
+          bsSize="large"
+          disabled={!validateForm()}
+          isLoading={isChanging}
+        >
+          Change Password
+        </LoaderButton>
+      </form>
+    </div>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
