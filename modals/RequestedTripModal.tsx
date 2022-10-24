436c571ba6d205fa07ff62016f7e0eed7cc30881
Jeremías <ramirezjeremias98@gmail.com>
2022-10-23 21:17:16 -0300

    feat: add getters

--- modals/RequestedTripModal.tsx
+++ modals/RequestedTripModal.tsx
@@ -1,8 +1,12 @@
 import React, { FC } from 'react';
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Pallete } from '../constants/Pallete';
 import { Modal, Text, Button } from 'react-native-paper';
 import { BasicModalProps } from '../models/props/basic-modal-props';
+import { Trip } from '../models/trip';
+import { TripsService } from '../services/TripsService';
+import { User } from '../models/user';
+import { UsersService } from '../services/UsersService';
 
 const styles = StyleSheet.create({
   container: {
@@ -15,7 +19,10 @@ const styles = StyleSheet.create({
    cancelButton: {
     marginTop: 20,
     borderColor: 'red'
-   }
+   },
+   acceptButton: {
+    marginTop: 10,
+   },
 });
 interface RequestedTripModalProps extends BasicModalProps {
     tripId: string,
@@ -25,12 +32,34 @@ const defaultStyles = {backgroundColor: 'white', padding: 20, margin: 10};
   
 export const RequestedTripModal: FC<RequestedTripModalProps> = ({visible, onDismiss, contentContainerStyle, tripId}: RequestedTripModalProps) => {
     
+    const [user, setUser] = React.useState<User | null>();
+    const [trip, setTrip] = React.useState<Trip | null>();
+
+    const loadData = async () => {
+        const trip: Trip | null = await TripsService.get(tripId);
+
+        if (trip) {
+            setTrip(trip);
+            const userId: number = Number.parseInt(trip.passengerId);
+            const user: User | null = await UsersService.getUser(userId);
+            setUser(user);
+        }
+    };
+
+    React.useEffect(() => {
+        loadData();
+    }, []);
+
     return (
         <>
             <Modal visible={visible} onDismiss={onDismiss} contentContainerStyle={{...defaultStyles, ...contentContainerStyle}}>
-                <Text variant="titleMedium" style={styles.title}>New trip available!</Text>
-                <Text>{tripId}</Text>
-                <Button style={styles.cancelButton} textColor='red' mode='outlined' onPress={() => console.log('Pressed')}>
+                {user ? <Text variant="titleSmall" style={styles.title}>{user.firstName} {user.lastName} wants to travel</Text> : <></>}
+                {trip ?  <Text variant="titleMedium">15 min</Text> : <></>}
+                {trip ?  <Text variant="displayLarge">$ {trip?.finalPrice}</Text> : <></>}
+                <Button style={styles.cancelButton} textColor='red' mode='outlined' onPress={onDismiss}>
+                    DECLINE
+                </Button>
+                <Button style={styles.acceptButton}  mode='contained' onPress={() => console.log('Pressed')}>
                     ACCEPT
                 </Button>
             </Modal>
