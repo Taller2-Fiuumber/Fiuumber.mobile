97e1f6fae2ed7065672c5e61aacc38613ac30a4b
Jeremías <ramirezjeremias98@gmail.com>
2022-12-01 23:54:39 -0300

    feat: califications

--- screens/TripScreen.tsx
+++ screens/TripScreen.tsx
@@ -1,29 +1,9 @@
 import * as React from 'react';
-import { StyleSheet } from "react-native";
-import { Text, View } from "../components/Themed";
-import { Pallete } from '../constants/Pallete';
-import { DirectionsBoxNative } from '../components/DirectionsBoxNative';
 import { User } from '../models/user';
 import { AuthService } from '../services/AuthService';
 import DriverHomeScreen from './DriverHomeScreen';
 import PassengerHomeScreen from './PassengerHomeScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: Pallete.greenBackground,
-  },
-  contentContainer: {
-    flex: 1,
-    backgroundColor: Pallete.greenBackground,
-  },
-  title: {
-    color: Pallete.darkColor,
-    textAlign: 'center',
-    fontSize: 18
-  },
-});
 export const TripScreen = () => {
 
   const user: User | undefined = AuthService.getCurrentUserToken()?.user;
@@ -33,11 +13,6 @@ export const TripScreen = () => {
       {user?.profile == "DRIVER" ?
         <DriverHomeScreen></DriverHomeScreen> :
         <PassengerHomeScreen></PassengerHomeScreen>
-        // <View style={styles.container}>
-        //   <View style={styles.contentContainer}>
-        //     <DirectionsBoxNative></DirectionsBoxNative>
-        //   </View>
-        // </View>
       }
     </>
   );
